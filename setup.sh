2abdc9d877022728f91062f24468bfeb69d7be5d
Benson Wong <mostlygeek+git@gmail.com>
2013-11-18 18:00:25 +0000

    snapshot

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DOT=.dot
+DIR=$HOME/$DOT
+
+# fetch or update dot repo
+if [[ ! -d "$DIR" ]]; then
+  # on initial setup, clone the repo
+  git clone git@github.com:jacobgroundwater/dot.git $DIR
+else
+  # force update against server
+  # we stash changes just in case you want them
+  (cd $DIR && git fetch --all && git stash && git reset --hard origin/master)
+fi
+
+# link everything to $HOME as a dotfile
+ls $DIR | while read f; do
+  echo -n "--> Linking $DIR/$f -> ~/.$f: " 
+  ln -sfn $DOT/$f $HOME/.$f &&\
+    echo "OK"         ||\
+    ecoh "FAIL"
+done
+
+echo "--- Done"
+
